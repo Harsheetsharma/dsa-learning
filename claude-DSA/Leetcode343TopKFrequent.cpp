733c6c5136816c9374b17b46e6297da2abf26173
Harsheetsharma <harshitsharma8012@gmail.com>
2026-05-02 17:02:42 +0530

    ....

--- claude-DSA/Leetcode343TopKFrequent.cpp
+++ claude-DSA/Leetcode343TopKFrequent.cpp
@@ -4,26 +4,29 @@
 #include <vector>
 using namespace std;
 
-void topKFrequent(int nums, int size, int k)
+void topKFrequent(int nums[], int size, int k)
 {
     unordered_map<int, int> mp;
 
+    // step1 count freq
     for (int i = 0; i < size; i++)
     {
         mp[nums[i]]++;
-        if (mp.size() > k)
-        {
-                }
     }
-    return;
+    for (auto it : mp)
+    {
+        if (it.second >= k)
+        {
+            cout << it.first;
+        }
+    }
 }
 
 int main()
 {
-    int nums[] = {1, 1, 1, 2, 2, 3};
+    int nums[] = {1, 1, 1, 2, 2, 3, 3};
     int size = sizeof(nums) / sizeof(nums[0]);
     int k = 2;
-    int ans = topKFrequent(nums, size, k);
-
+    topKFrequent(nums, size, k);
     return 0;
 }
